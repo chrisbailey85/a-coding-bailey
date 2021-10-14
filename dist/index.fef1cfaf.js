e1aa7269daa913091eb23ea8c723ebc363e1c624
Chris Bailey <chris85bailey@aol.co.uk>
2021-10-14 17:22:38 +0100

    js for toggle mode and links eventListeners done, css styles done for 320px and 375px

--- dist/index.fef1cfaf.js
+++ dist/index.fef1cfaf.js
@@ -458,10 +458,14 @@ function hmrAcceptRun(bundle, id) {
 const mobileMenuBtn = document.querySelector('.hamburger');
 const navigation = document.querySelector('.nav');
 const navLinks = document.querySelectorAll('.nav__link');
+const body = document.querySelector('body');
+const toggleModeBtn = document.querySelector('.toggle__mode');
+const html = document.querySelector('html');
 mobileMenuBtn.addEventListener('click', ()=>{
     mobileMenuBtn.classList.toggle('is-active');
     mobileMenuBtn.classList.contains('is-active') ? mobileMenuBtn.ariaExpanded = true : mobileMenuBtn.ariaExpanded = false;
     navigation.classList.toggle('open');
+    body.classList.toggle('nav--open');
 });
 navLinks.forEach((link)=>{
     link.addEventListener('click', ()=>{
@@ -469,8 +473,13 @@ navLinks.forEach((link)=>{
             navigation.classList.remove('open');
             mobileMenuBtn.ariaExpanded = false;
             mobileMenuBtn.classList.remove('is-active');
+            body.classList.remove('nav--open');
         }
     });
+});
+toggleModeBtn.addEventListener('click', ()=>{
+    html.classList.toggle('dark--mode');
+    html.classList.contains('dark--mode') ? toggleModeBtn.innerHTML = '<i class="far fa-moon icon"></i>' : toggleModeBtn.innerHTML = '<i class="far fa-sun icon"></i>';
 });
 
 //# sourceMappingURL=index.fef1cfaf.js.map
