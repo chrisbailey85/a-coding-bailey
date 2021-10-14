1022df1f5e7e03f8573412d740b67b832254f333
Chris Bailey <chris85bailey@aol.co.uk>
2021-10-14 13:19:32 +0100

    css done for navbar, and js done for nav, css done up to services section

--- dist/index.fef1cfaf.js
+++ dist/index.fef1cfaf.js
@@ -456,9 +456,21 @@ function hmrAcceptRun(bundle, id) {
 
 },{}]},["2JWsR"], null, "parcelRequire11b4")
 const mobileMenuBtn = document.querySelector('.hamburger');
+const navigation = document.querySelector('.nav');
+const navLinks = document.querySelectorAll('.nav__link');
 mobileMenuBtn.addEventListener('click', ()=>{
     mobileMenuBtn.classList.toggle('is-active');
     mobileMenuBtn.classList.contains('is-active') ? mobileMenuBtn.ariaExpanded = true : mobileMenuBtn.ariaExpanded = false;
+    navigation.classList.toggle('open');
+});
+navLinks.forEach((link)=>{
+    link.addEventListener('click', ()=>{
+        if (navigation.classList.contains('open')) {
+            navigation.classList.remove('open');
+            mobileMenuBtn.ariaExpanded = false;
+            mobileMenuBtn.classList.remove('is-active');
+        }
+    });
 });
 
 //# sourceMappingURL=index.fef1cfaf.js.map
--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 const mobileMenuBtn = document.querySelector('.hamburger');
-
-
+const navigation = document.querySelector('.nav');
+const navLinks = document.querySelectorAll('.nav__link');
 mobileMenuBtn.addEventListener('click', () => {
     mobileMenuBtn.classList.toggle('is-active');
     mobileMenuBtn.classList.contains('is-active') ? mobileMenuBtn.ariaExpanded = true : mobileMenuBtn.ariaExpanded = false;
+    navigation.classList.toggle('open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        if(navigation.classList.contains('open')) {
+            navigation.classList.remove('open') 
+            mobileMenuBtn.ariaExpanded = false;
+            mobileMenuBtn.classList.remove('is-active')
+        }
+    })
 })
